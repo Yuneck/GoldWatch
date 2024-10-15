d0fa84ab319aac6b8dcc56f2072cb157a13c7fe3
Jan Garbiec <71603567+Yuneck@users.noreply.github.com>
2024-10-15 19:24:46 +0200

    SecondMachineTest

--- GoldWatchProject/App.js
+++ GoldWatchProject/App.js
@@ -6,7 +6,7 @@ console.log("App executed now");
 
   return (
     <View style={styles.container}>
-      <Text>TEST ZŁOTO223 TEST</Text>
+      <Text>TEST ZŁOTO225 TEST</Text>
       <StatusBar style="auto" />
     </View>
   );
