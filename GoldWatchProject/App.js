9141d6cd0938453e43852d9b66a88dffcc1aefc1
marek0902 <marek090273@gmail.com>
2024-10-19 21:47:27 +0200

    2

--- GoldWatchProject/App.js
+++ GoldWatchProject/App.js
@@ -1,6 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import WelcomeScreen from './app/screens/WelcomeScreen';
+
 export default function App() {
 console.log("App executed now");
 
