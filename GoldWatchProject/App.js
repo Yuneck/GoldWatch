2e145ca0463d0d3ba9da35a57a47741ae3e2bcc0
marek0902 <marek090273@gmail.com>
2024-10-19 21:49:01 +0200

    3

--- GoldWatchProject/App.js
+++ GoldWatchProject/App.js
@@ -14,7 +14,6 @@ import WelcomeScreen from './app/screens/WelcomeScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
-
   return (
     <WelcomeScreen></WelcomeScreen>
 
