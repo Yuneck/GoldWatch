c43a2861a1ecae83aeedebf178c68db75d5c3071
marek0902 <marek090273@gmail.com>
2024-10-19 21:43:44 +0200

    v1

---
+++ GoldWatchProject/app/screens/WelcomeScreen.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { ImageBackground, StyleSheet, View } from 'react-native';
+
+function WelcomeScreen(props) {
+    return (
+        <View style={styles.container}>
+            <ImageBackground 
+            resizeMode="contain"            
+            style= {styles.background}
+            source={require('../assets/goldbar.png')}
+            >
+                <View style={styles.myAssets}></View>
+                <View style={styles.registerAssets}></View>
+
+            </ImageBackground>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    background: {
+        flex: 1,
+        justifyContent: "flex-end",
+    },
+    container: {
+        backgroundColor: "black",
+        flex: 1,
+    },
+    myAssets: {
+        width: "20%",
+        height: 70,
+        backgroundColor: "gold",
+    }, 
+    registerAssets: {
+        width: "20%",
+        height: 70,
+        backgroundColor: "gold",
+    }
+
+})
+export default WelcomeScreen;
