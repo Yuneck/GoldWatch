dc4d225125e34148425d79cf4f07918ebe42b8d0
marek0902 <marek090273@gmail.com>
2024-10-26 11:13:37 +0200

    2

--- GoldWatchProject/App.js
+++ GoldWatchProject/App.js
@@ -4,40 +4,20 @@ import WelcomeScreen from './app/screens/WelcomeScreen';
 import { StyleSheet, Text, View} from 'react-native';
 import SQLite from 'react-native-sqlite-storage';
 
+const db = SQLite.openDatabase(
+  {
+    name: 'testDB',
+    location: 'default',
+  },
+  () => {
+    console.log('Database opened successfully');
+  },
+  error => {console.log(error)}
+);
+
+
+
 export default function App() {
-
-  /*
-  const [dbInstance, setDb] = useState(null);  // Track database status
-
-  useEffect(() => {
-    dbInstance = SQLite.openDatabase(
-      {
-        name: 'testGoldWatchDB',
-        location: 'default',
-      },
-      () => {
-        console.log('Database opened successfully');
-        setDb(dbInstance); // Set db only after successful opening
-      },
-      error => {
-        console.error('Error opening DB:', error);
-        // Handle the error appropriately, maybe display a message to the user.
-      }
-    );
-  
-        // Clean up the database connection when the component unmounts. This is crucial!
-        return () => {
-          if (dbInstance) {
-            dbInstance.close();
-          }
-        };
-      }, []);
-    
-      // Now, conditionally render based on db being ready:
-      if (!dbInstance) {
-        return <Text>Loading database...</Text>; // Or a better loading indicator
-      }
-*/
 
   return (
 
@@ -53,4 +33,3 @@ const styles = StyleSheet.create({
   //  paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
   },
 });
-
