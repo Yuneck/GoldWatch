fc491b48f859a85a89efad3d6d11c613de162946
marek0902 <marek090273@gmail.com>
2024-10-26 12:11:20 +0200

    7

--- GoldWatchProject/App.js
+++ GoldWatchProject/App.js
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import { StyleSheet, Text, View} from 'react-native';
-import SQLite from 'react-native-sqlite-storage';
+
 
 
 export default function App() {
