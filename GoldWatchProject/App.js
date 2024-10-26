e8d0a22b79522a0c061b9c7eb61127962aa0cfce
marek0902 <marek090273@gmail.com>
2024-10-26 10:00:06 +0200

    1

--- GoldWatchProject/App.js
+++ GoldWatchProject/App.js
@@ -1,62 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import { Text, View } from 'react-native';
-import SQLite from 'react-native-sqlite-storage';
-import { openDatabase } from 'react-native-sqlite-storage';
 
-const App = () => {
-  const [db, setDb] = useState(null);
-  const [dbStatus, setDbStatus] = useState('Connecting...');
-  const [errorMessage, setErrorMessage] = useState('');
-
-
-  useEffect(() => {
-    const dbName = 'myDatabase.db'; // Choose a database name
-    let dbInstance = null;
-
-    const openDB = async () => {
-      try {
-        dbInstance = await SQLite.openDatabase({ name: dbName, location: 'default' });
-        setDb(dbInstance);
-        setDbStatus('Database opened successfully');
-      } catch (error) {
-        setDbStatus('Error opening database');
-        setErrorMessage(error.message);
-        console.error("Error opening DB:", error);
-      }
-    };
-
-    openDB();
-
-    return () => {
-      if (dbInstance) {
-        dbInstance.close().then(() => {
-            console.log('Database closed successfully');
-        }).catch((error) => {
-            console.error('Error closing database:', error);
-        });
-      }
-    };
-  }, []);
-
-
-  return (
-    <View>
-      <Text>{dbStatus}</Text>
-      {errorMessage && <Text style={{ color: 'red' }}>{errorMessage}</Text>}
-      {db && <Text>Database object: {JSON.stringify(db)}</Text>} 
-    </View>
-  );
-};
-export default App;
-
-
-/*
 import React, { useState, useEffect } from 'react';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import { StyleSheet, Text, View} from 'react-native';
 import SQLite from 'react-native-sqlite-storage';
 
 export default function App() {
+
+  /*
   const [dbInstance, setDb] = useState(null);  // Track database status
 
   useEffect(() => {
@@ -87,7 +37,7 @@ export default function App() {
       if (!dbInstance) {
         return <Text>Loading database...</Text>; // Or a better loading indicator
       }
-
+*/
 
   return (
 
@@ -103,4 +53,4 @@ const styles = StyleSheet.create({
   //  paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
   },
 });
-*/
+
