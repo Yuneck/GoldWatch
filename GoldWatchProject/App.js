06f64b56dfd53c95c7e2a212a33585522f20e274
marek0902 <marek090273@gmail.com>
2024-10-27 22:22:00 +0100

    1

--- GoldWatchProject/App.js
+++ GoldWatchProject/App.js
@@ -3,7 +3,16 @@ import React from 'react';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import { StyleSheet, Text, View} from 'react-native';
 
-
+const db = SQLite.openDatabase(
+  {
+    name: 'testDB',
+    location: 'default',
+  },
+  () => {
+    console.log('Database opened successfully');
+  },
+  error => {console.log(error)}
+);
 
 export default function App() {
 
